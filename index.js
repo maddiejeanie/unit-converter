6ac36c2a3c27749aefd21cdb9a89e1137e88cd06
maddiejeanie <69501808+maddiejeanie@users.noreply.github.com>
2022-08-08 18:05:39 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const convertmtof = 3.281
+const convertltog = 0.264 
+const convertptok = 2.204 
+//`${inputEl.value} meters = ${feet} | ${inputEl.value} feet = ${meters}`
+
+const convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input-el")
+
+convertBtn.addEventListener("click", function(){
+
+meterstofeet.innerHTML = `${ inputEl.value } metres = ${ (inputEl.value * convertmtof).toFixed(3)  } feet <br> ${ inputEl.value } feet = ${ (inputEl.value / convertmtof).toFixed(3) } metres`
+
+literstogallons.innerHTML = `${ inputEl.value } liters = ${(inputEl.value * convertltog).toFixed(3) } gallons <br> ${ inputEl.value } gallons = ${ (inputEl.value / convertltog).toFixed(3) } liters`
+
+kilostopounds.innerHTML = `${ inputEl.value } kilograms = ${ (inputEl.value * convertptok).toFixed(3) } pounds <br> ${ inputEl.value } pounds = ${ (inputEl.value / convertptok).toFixed(3) } kilograms`
+}
+
+)
+
